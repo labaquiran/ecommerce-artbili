ee267305777d19f4fa7cc7413ab9c80e0e9a539b
LA Baquiran <labaquiran@gmail.com>
2026-06-18 23:26:41 +0800

    Repo setup commit

---
+++ app/components/root-navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function RootNavbar() {
+  return (
+    <div className="w-full h-16 bg-white text-black border-b border-gray-200 flex items-center justify-between px-4 ">
+      <Link href="/">ARTBILI</Link>
+      <div className="flex space-x-4">
+        <Link href="/">Sign In</Link>
+        <Link href="/">Cart</Link>
+      </div>
+    </div>
+  );
+}
